084918edce57b0e27879c06fa271177dfe8a088c
joedenby <jd744@kent.ac.uk>
2023-03-08 14:08:28 +0000

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using FileShare;
+
+Console.WriteLine($"Permissions: {Connection.AssignPrivillages()}");
+await FileTransmission.Send(@"C:\Users\Joseph\Documents\hello.gif", "105.107.58.215", 8888);
